fd0b8b439973685b3ae8d890b29280bacff5c9ce
Brian Luong <brian@satsuma.xyz>
2023-08-14 17:19:05 -0700

    whitespace trimmer

--- src/strings.rs
+++ src/strings.rs
@@ -6,6 +6,10 @@ fn main() {
     println!("count_chars_2: {}", count_chars_2(s.to_string()));
     println!("count_char_in_string: {}", count_char_in_string(s, 'l'));
     println!("join_strings: {}", join_strings(vec![s, s2], '!'));
+    println!(
+        "trim_whitespace: {}",
+        trim_whitespace(format!("    {s}       ").as_str())
+    );
 }
 
 fn count_chars(s: &str) -> i32 {
@@ -44,4 +48,32 @@ fn join_strings(v: Vec<&str>, delimeter: char) -> String {
     }
 
     final_str
+}
+
+fn trim_whitespace(s: &str) -> String {
+    // Trim leading whitespace
+    let mut first_pass_str = String::new();
+    let mut reached_non_whitespace: bool = false;
+    for c in s.chars() {
+        if reached_non_whitespace {
+            first_pass_str.push_str(c.to_string().as_str());
+        } else if !c.is_whitespace() {
+            reached_non_whitespace = true;
+            first_pass_str.push_str(c.to_string().as_str());
+        }
+    }
+
+    // Trim trailing whitespace
+    let mut second_pass_str = String::new();
+    reached_non_whitespace = false;
+    for c in first_pass_str.chars().rev() {
+        if reached_non_whitespace {
+            second_pass_str = format!("{}{}", c.to_string(), second_pass_str);
+        } else if !c.is_whitespace() {
+            reached_non_whitespace = true;
+            second_pass_str = format!("{}{}", c.to_string(), second_pass_str);
+        }
+    }
+
+    second_pass_str
 }
