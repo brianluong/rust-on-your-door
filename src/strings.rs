d33dc33d88de6d947df5754d3d01baacec2a5c03
Brian Luong <brian@satsuma.xyz>
2023-08-15 09:28:00 -0700

    merge sort

--- src/strings.rs
+++ src/strings.rs
@@ -10,6 +10,7 @@ fn main() {
         "trim_whitespace: {}",
         trim_whitespace(format!("    {s}       ").as_str())
     );
+    println!("merge_sort, {:?}", merge_sort(vec!["b", "a", "c", "b"]));
 }
 
 fn count_chars(s: &str) -> i32 {
@@ -76,4 +77,57 @@ fn trim_whitespace(s: &str) -> String {
     }
 
     second_pass_str
+}
+
+fn merge_sort(mut v: Vec<&str>) -> Vec<&str> {
+    let len = v.len();
+    v = merge_sort_helper(v, 0, len - 1);
+    v
+}
+
+fn merge_sort_helper(mut v: Vec<&str>, start: usize, end: usize) -> Vec<&str> {
+    if start >= end {
+        return v;
+    }
+
+    let mid = (start + end) / 2;
+
+    v = merge_sort_helper(v, start, mid);
+    v = merge_sort_helper(v, mid + 1, end);
+    v = merge(v, start, mid, end);
+    v
+}
+
+fn merge(mut v: Vec<&str>, start: usize, mid: usize, end: usize) -> Vec<&str> {
+    let mut temp_vec = Vec::new();
+    let mut i = start;
+    let mut j = mid + 1;
+
+    while i <= mid && j <= end {
+        if v[i] < v[j] {
+            temp_vec.push(v[i]);
+            i += 1
+        } else {
+            temp_vec.push(v[j]);
+            j += 1;
+        }
+    }
+
+    while i <= mid {
+        temp_vec.push(v[i]);
+        i += 1
+    }
+
+    while j <= end {
+        temp_vec.push(v[j]);
+        j += 1;
+    }
+
+    let mut temp_vec_i = 0;
+    for i in start..end + 1 {
+        v[i] = temp_vec[temp_vec_i];
+        temp_vec_i += 1;
+    }
+
+    v
 }
